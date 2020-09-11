2d272feabcb4fa61fd9fcc481c3207a8467d9b62
Connor Bode <connor.bode@gmail.com>
2020-09-11 18:25:42 -0400

    v0.0.2

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ echo "Current version is $(cat VERSION.txt).  What version did you want to relea
 read VERSION
 echo $VERSION > VERSION.txt
 python setup.py sdist
-twine upload "dist/django_simple_static_file_storage-$VERSION.tar.gz"
+twine upload "dist/django_heroicons-$VERSION.tar.gz"
 git add .
 git commit -m "v$VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
